415df2f9384d257f10f50e02c0428c8843eadde1
Hanumanthe Gowda <hanu.unah@gmail.com>
2022-01-23 19:18:54 +0530

    refactor promotion engine tests

--- Source/Promotion.Engine.UnitTests/Domain/PromotionEngineTest.cs
+++ Source/Promotion.Engine.UnitTests/Domain/PromotionEngineTest.cs
@@ -1,4 +1,5 @@
-﻿using NFluent;
+﻿using Moq;
+using NFluent;
 using Promotion.Engine.Domain.Interfaces;
 using Promotion.Engine.Domain.Models;
 using Promotion.Engine.UnitTests.TestData;
@@ -9,17 +10,21 @@ namespace Promotion.Engine.UnitTests.Domain
     public class PromotionEngineTest
     {
         private readonly IPromotionEngine _target;
+        private readonly Mock<IPromotionProvider> _promotionProviderMock;
         public PromotionEngineTest()
         {
-            // Ideally promotionProvider should be mocked using Moq, due time constraint, would use this test as integration test
-            var promotionProvider = new PromotionProvider();
-            _target = new PromotionEngine(promotionProvider);
+            _promotionProviderMock = new Mock<IPromotionProvider>();
+            _target = new PromotionEngine(_promotionProviderMock.Object);
         }
 
         [Theory]
         [MemberData(nameof(PromotionEngineDataGenerator.GetData), MemberType = typeof(PromotionEngineDataGenerator))]
         public void CalculateTotal_ValidOrder_CalculateTotalOrderValue(Order order, double expected)
         {
+            _promotionProviderMock.Setup(x => x.GetPromotion(PromotionType.Type1)).Returns(new PromotionType1());
+            _promotionProviderMock.Setup(x => x.GetPromotion(PromotionType.Type2)).Returns(new PromotionType2());
+            _promotionProviderMock.Setup(x => x.GetPromotion(PromotionType.Type3)).Returns(new PromotionType3());
+
             _target.CalculateTotal(order);
             Check.That(order.Total).Equals(expected);
         }
