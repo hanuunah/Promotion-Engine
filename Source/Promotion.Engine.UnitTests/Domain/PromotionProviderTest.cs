b6bd054cf92df165ddc4f3629cdc763c15561a56
Hanumanthe Gowda <hanu.unah@gmail.com>
2022-01-23 14:49:03 +0530

    added test cases promotion provider

--- Source/Promotion.Engine.UnitTests/Domain/PromotionProviderTest.cs
+++ Source/Promotion.Engine.UnitTests/Domain/PromotionProviderTest.cs
@@ -1,8 +1,40 @@
-﻿
+﻿using NFluent;
+using Promotion.Engine.UnitTests.TestData;
+using PromotionEngine.Domain.Interfaces;
+using PromotionEngine.Domain.Models;
+using Xunit;
 
 namespace Promotion.Engine.UnitTests.Domain
 {
     public class PromotionProviderTest
     {
+        private readonly IPromotionProvider _target;
+        public PromotionProviderTest()
+        {
+
+            _target = new PromotionProvider();
+        }
+
+        [Theory]
+        [ClassData(typeof(TestDataGenerator))]
+        public void GetPromotion_ValidPromotionType_ReturnPromotionType(PromotionType promotionType)
+        {
+            var promotion = _target.GetPromotion(promotionType);
+            Check.That(promotion).IsNotNull();
+        }
+
+        [Fact]
+        public void GetPromotion_InvalidValidPromotionType_ReturnNull()
+        {
+            var promotion = _target.GetPromotion(new PromotionType(999, "Invalid"));
+            Check.That(promotion).IsNull();
+        }
+
+        [Fact]
+        public void GetPromotion_NullArgument_ReturnNull()
+        {
+            var promotion = _target.GetPromotion(null);
+            Check.That(promotion).IsNull();
+        }
     }
 }
---
+++ Source/Promotion.Engine.UnitTests/TestData/TestDataGenerator.cs
@@ -0,0 +1,31 @@
+﻿using PromotionEngine.Domain.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Promotion.Engine.UnitTests.TestData
+{
+    public class TestDataGenerator : IEnumerable<object[]>
+    {
+        private readonly List<object[]> _data = new List<object[]>
+    {
+        new object[] { PromotionType.Type1},
+        new object[] { PromotionType.Type2}
+    };
+
+        public IEnumerator<object[]> GetEnumerator() => _data.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        public static IEnumerable<object[]> GetPromotionTypeFromDataGenerator()
+        {
+            yield return new object[]
+            {
+           PromotionType.Type2,
+            };
+
+        }
+
+    }
+}
