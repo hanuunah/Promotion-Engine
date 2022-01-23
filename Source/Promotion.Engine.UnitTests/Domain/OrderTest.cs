9ff6522c2e72f938f93699c6cd4a33426cb89acd
Hanumanthe Gowda <hanu.unah@gmail.com>
2022-01-23 13:47:02 +0530

    added tests for order

--- Source/Promotion.Engine.UnitTests/Domain/OrderTest.cs
+++ Source/Promotion.Engine.UnitTests/Domain/OrderTest.cs
@@ -1,5 +1,8 @@
 ﻿
-
+using NFluent;
+using PromotionEngine.Domain.Interfaces;
+using PromotionEngine.Domain.Models;
+using System.Collections.Generic;
 using Xunit;
 
 namespace Promotion.Engine.UnitTests.Domain
@@ -7,21 +10,35 @@ namespace Promotion.Engine.UnitTests.Domain
     public class OrderTest
     {
 
+        private readonly IOrder _target;
         public OrderTest()
         {
-            
+            var promoTypes = new List<PromotionType>
+            {
+                PromotionType.Type1,
+                PromotionType.Type2,
+                PromotionType.Type3,
+            };
+
+            _target = new Order(1, promoTypes);
         }
 
         [Fact]
         public void AddItem_ValidItem_AddItemToOrder()
         {
-          
+            var skuA = new Sku("A", 50);
+            var itemA = new OrderItem(1, skuA, 5);
+            _target.AddItem(itemA);
+
+            Check.That(_target.Items).HasSize(1);
         }
 
         [Fact]
-        public void AddItem_InvalidValidItem_ExceptionThrown()
+        public void AddItem_InvalidValidItem_FailedToAddItemToOrder()
         {
+            _target.AddItem(null);
 
+            Check.That(_target.Items).HasSize(0);
         }
 
     }
